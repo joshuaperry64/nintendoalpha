ebd90734aadc90f809ff4d97704525737afa41f3
Joshua Stephen Perry <josh64perry@gmail.com>
2026-03-10 16:45:06 -0400

    Delete languages/FR_fr directory

--- languages/FR_fr/tools/Storage/launch_emuGUIibo.bat
+++
@@ -1,9 +0,0 @@
-goto:%~1
-
-:display_title
-title Lancer emuGUIibo %this_script_version% - Shadow256 Ultimate Switch Hack Script %ushs_version%
-goto:eof
-
-:intro
-echo Ce script permet juste de lancer emuGUIibo, un logiciel permettant de créer des amiibo pour le module Emuiibo.
-goto:eof
